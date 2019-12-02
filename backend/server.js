ab4331b5f343cf9b9397f00d05f4d7fcac3e8aaa
CodyThomasCrump <codytcrump@gmail.com>
2019-12-02 12:15:05 -0600

    Added the DataSchema and worked on App.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const bodyParser = require('body-parser');
 //Import the getSecret function from the secret.js file
 const getSecret = require('./secret');
 
+//Import the Mongoose Schema for our data
+const Data = require('./DataSchema');
+
 //Constant to hold the port that we are going to use to connect
 const API_PORT = 3001;
 
@@ -23,11 +26,9 @@ const router = express.Router();
 mongoose.connect(getSecret('dbUrl'), { useNewUrlParser: true, useFindAndModify: false });
 
 //Reference to our database connection
-
 let db = mongoose.connection;
 
-//Use the database connection to print out an error if one occurs when we try to connect to the database
-
+//Use the database connection to print out an error if one occurs when we try to connect to the database 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 //Configure body-parser and morgan
@@ -39,6 +40,50 @@ app.use(logger('dev'));
 router.get('/', (req, res) => {
     res.json({ message: 'HELLO WORLD' });
 })
+
+//Route to retrieve data from the database
+router.get('/getData', (req, res) => {
+    //Use Mongoose to find data with the given schema  
+    Data.find((err, data) => {
+        console.log(data);
+
+        if (err) {
+            return res.json({ success: false, error: err });
+        }
+
+        return res.json({ success: true, data: data });
+    });
+});
+
+//Route that will pull new information in the database
+router.post('/postData', (req, res) => {
+    //A new instance of the Mongoose Schema which we will send to the database
+    let newData = new Data();
+
+    //Pull the ID and message from the body of the request
+    const { id, message } = req.body;
+
+    //If ID does not have a value and is not equal to 0 or message doesn't have a value, return an error
+    if ((!id && id !== 0) || !message) {
+        return res.json({
+            success: false,
+            error: 'INVALID INPUT'
+        })
+    }
+
+    //Configure the schema object
+    newData.id = id;
+    newData.message = message;
+
+    //Try to save to the database 
+    newData.save(err => {
+        if (err) {
+            return res.json({ success: false, error: err });
+        }
+
+        return res.json({ success: true });
+    });
+});
 
 //Tell Express to use a certain path and to use the router we set up
 app.use('/api', router);
